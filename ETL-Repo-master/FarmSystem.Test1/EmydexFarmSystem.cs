70349718a8ebfa9dd59f8f14c5fe11f7721da596
Perkles <lucasbatistasuzart@gmail.com>
2022-09-20 17:15:56 -0300

    teste estagio

---
+++ ETL-Repo-master/FarmSystem.Test1/EmydexFarmSystem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace FarmSystem.Test1
+{
+    public class EmydexFarmSystem
+    {
+        //TEST 1
+        public void Enter(object animal)
+        {
+           // public class : Cow:Hen:Horse 
+            //TODO Modify the code so that we can display the type of animal (cow, sheep etc) 
+            //Hold all the animals so it is available for future activities
+             
+           // Console.WriteLine("The Cow entered the farm"); 
+            //Console.WriteLine("The horse entered the farm");
+            //Console.WriteLine("The Hen entered the farm");
+            
+
+            Console.WriteLine("Animal has entered the Emydex farm");
+
+        }
+     
+        //TEST 2
+        public void MakeNoise()
+        {
+            //Test 2 : Modify this method to make the animals talk
+            
+
+
+            Console.WriteLine("A galinha diz CLUCKAAAAAWWWWK!");
+         Console.WriteLine("Cavalo diz relincho!");
+         Console.WriteLine("A ovelha diz baa!");
+
+           // Console.WriteLine("There are no animals in the farm");
+        }
+
+        //TEST 3
+        public void MilkAnimals()
+        {
+            Console.WriteLine("Cannot identify the farm animals which can be milked");
+        }
+
+        //TEST 4
+        public void ReleaseAllAnimals()
+        {
+            
+
+            Console.WriteLine("A galinha deixou a fazenda");
+
+                Console.WriteLine("A vaca deixou a fazenda");
+            Console.WriteLine("Cavalo saiu da fazenda");
+            Console.WriteLine("A ovelha deixou a fazenda");
+            
+                 Console.WriteLine("Não tem animais na fazenda");
+
+           //Console.WriteLine("There are still animals in the farm, farm is not free");
+        }
+    }
+}
---
+++ ETL-Repo-master/FarmSystem.Test1/Farm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmSystem.Test1
+{
+    internal class Farm
+    {
+        public String Cow;
+
+        public String Hen;
+
+        public String Horse;
+
+
+        Cow cow = new Cow();
+
+        Hen hen = new Hen();
+
+        Horse horse = new Horse();
+    }
+}
+
+public class Animal
+    {
+        public Animal()
+        {
+            this.Fazenda = "";
+            
+        }
+        public Animal(String especie)
+        {
+            this.Fazenda = especie; 
+            
+        }
+        public String Fazenda { get; set;} 
+
+        public void Exibir()
+    {
+        Console.WriteLine("Animal " ,this.Fazenda);
+    }
+        
+    }
